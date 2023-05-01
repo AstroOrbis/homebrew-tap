95b61fa367c4555cc819cef5dcee11cd619f9a93
Astro Orbis <astroorbis@gmail.com>
2023-05-01 08:59:26 -0400

    Compoundcalc edit

--- Formula/compoundcalc.rb
+++ Formula/compoundcalc.rb
@@ -1,5 +1,5 @@
 class Compoundcalc < Formula
-  desc "A CLI tool to easily calculate compound interest"
+  desc "Compound interest CLI tool"
   homepage "https://github.com/AstroOrbis/compoundcalc"
   url "https://github.com/AstroOrbis/compoundcalc/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "c9936f014dd7a9e8968413234a6b562aa58b66dad14aec72248735d374eb3684"
@@ -9,7 +9,7 @@ class Compoundcalc < Formula
   depends_on "rust" => :build
 
   def install
-    system "cargo", "build", "--release", "--bin", "compoundcalc"
+    system "cargo", "build", "-r"
     bin.install "target/release/compoundcalc"
   end
 end
