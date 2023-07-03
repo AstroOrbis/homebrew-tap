d7c7843ad0cea7a8a8e9d1991bc77a0b5b03276a
Astro Orbis <astroorbis@gmail.com>
2023-07-03 07:47:51 -0400

    I think I'm dumb

--- Formula/hyfetch.rb
+++ Formula/hyfetch.rb
@@ -9,6 +9,6 @@ class Hyfetch < Formula
 	depends_on "python@3.11" => :build
 
 	def install
-	  system "pip", "install", "git+https://github.com/hykilpikonna/hyfetch.git@master"
+	  system "pip", "install", "."
 	end
   end
