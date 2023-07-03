fefdf92268753f7624815b15c569678ad4841c71
Astro Orbis <astroorbis@gmail.com>
2023-07-03 07:46:42 -0400

    Hyfetch test

--- Formula/hyfetch.rb
+++ Formula/hyfetch.rb
@@ -1,7 +1,10 @@
 class Hyfetch < Formula
 	desc "neofetch with pride flags <3"
 	homepage "https://github.com/hykilpikonna/hyfetch"
+	url "https://github.com/hykilpikonna/hyfetch/archive/refs/tags/1.4.9.tar.gz"
+	sha256 "4c7359db32aaeb91031b32f7ed28f0fdf0b8d2e6790c6b1e069153b265c54664"
 	license "MIT"
+	head "https://github.com/hykilpikonna/hyfetch.git"
 
 	depends_on "python@3.11" => :build
 
